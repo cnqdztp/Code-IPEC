96d3d772059ed3f337711add257c8b039a72002e
气泡缓释胶囊 <japanztp@gmail.com>
2022-03-12 17:41:24 +0800

    Pre-git for android build

---
+++ Assets/DevConsole.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevConsole : MonoBehaviour
+{
+    //#if !UNITY_EDITOR
+    static string myLog = "";
+    private string output;
+    private string stack;
+
+    private void Start()
+    {
+    }
+    void OnEnable()
+    {
+        // Debug.Log("Debug Console Running");
+        Application.logMessageReceived += Log;
+    }
+     
+    void OnDisable()
+    {
+        Application.logMessageReceived -= Log;
+    }
+     
+    public void Log(string logString, string stackTrace, LogType type)
+    {
+        output = logString;
+        stack = stackTrace;
+        myLog = output + "\n" + myLog;
+        if (myLog.Length > 5000)
+        {
+            myLog = myLog.Substring(0, 4000);
+        }
+    }
+     
+    void OnGUI()
+    {
+        //if (!Application.isEditor) //Do not display in editor ( or you can use the UNITY_EDITOR macro to also disable the rest)
+        {
+            myLog = GUI.TextArea(new Rect(10, 10, Screen.width - 10, Screen.height - 10), myLog);
+        }
+    }
+    //#endif
+
+    
+}
